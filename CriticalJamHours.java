0f71e4c249ecf8e03877984bf8af71847639663e
prateek_pokhrel <prateekpokhrel000@gmail.com>
2025-05-18 01:56:38 +0530

    Add files via upload

---
+++ CriticalJamHours.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class CriticalJamHours
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n =sc.nextInt();
+    int[] vehicles = new int[n];
+
+    for(int i =0; i<n; i++)
+    {
+      vehicles[i] = sc.nextInt();
+    }
+    int count =0;
+    for(int i =1; i<n; i++)
+    {
+      if(vehicles[i] >=50 && vehicles[i] > vehicles[i-1])
+      {
+        count++;
+      }
+    }
+    System.out.println(count);
+  }
+}
---
+++ IceCreamSelector.java
@@ -0,0 +1,22 @@
+public class IceCreamSelector {
+
+    public static boolean canBuyIceCream(int budget, int[] iceCreamPrices) {
+        for (int price : iceCreamPrices) {
+            if (price == budget) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        int budget = 30;
+        int[] iceCreamPrices = {10, 25, 30, 50};
+
+        boolean result = canBuyIceCream(budget, iceCreamPrices);
+        
+        System.out.println(result); 
+         // Output: true
+    }
+}
